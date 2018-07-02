4b761cc43483c049c7820611416fec47f4c60241
Doctorgrezz <40708056+Doctorgrezz@users.noreply.github.com>
2018-07-01 19:23:07 -0700

    Add files via upload

---
+++ src/js/data/2018-07-06.js
@@ -0,0 +1,17 @@
+dataSetVersion = "2018-07-06"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Bren Ten",
+    img: "N0UY8qV.png",
+  },
+  {
+    name: "FNP-9",
+    img: "43JLCcX.png",
+  },
+  {
+	name: "M1911",
+	img: "OP5ZCPT.png",
+  }
+];
